a720903f96a04ffe3497b939f1c1558b2e399814
vbrh-immalle <hans.vanbroeckhoven@immalle.be>
2020-03-01 16:55:44 +0100

    Initial commit

---
+++ main.py
@@ -0,0 +1,14 @@
+
+som = 0
+for i in range(1, 11):
+    som += i
+    print(f'i: {i} - tussentijdse som: {som}')
+print(f'totaalsom: {som}')
+
+namen = ['Jos', 'Jan', 'Mieke']
+
+for i in range(0, len(namen)):
+    print(f'naam {i} = {namen[i]}')
+
+for naam in namen:
+    print(naam)
